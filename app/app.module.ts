caf8b3446de49e49427b2c602648c27bbdc789f1
Frank <frankmanzhu@hotmail.com>
2016-10-08 20:12:48 +1100

    add in rest service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -13,6 +14,7 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
